d3ff1b8d3a18cdd8ee8b63d68adbb5bbce418dd0
Cicero Nascimento <antoniocicero1@live.com>
2022-05-03 21:29:18 -0300

    Initial Commit

---
+++ ChallengeMvp/ChallengeMvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeMvpApp.swift
+//  ChallengeMvp
+//
+//  Created by Cicero Nascimento on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeMvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChallengeMvp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChallengeMvp
+//
+//  Created by Cicero Nascimento on 03/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
